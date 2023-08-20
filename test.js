1d4b8c8ac010239e5a1940bad80fb6c4b9c1c46b
tihon228supermegaproIT <119882092+tihon228supermegaproIT@users.noreply.github.com>
2023-08-20 22:53:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
-const arr = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ], [ 10, 12, 11 ] ];
+function towelSort(matrix) {
+  if (matrix === undefined || matrix.length === 0) {
+    return [];
+  }
+  
+  for (let i = 1; i < matrix.length; i += 2) {
+    matrix[i] = matrix[i].reverse();
+  }
+  
+  return matrix.flat();
+}
 
-console.log(
-  arr.reduce((acc, cur, i) => {
-    cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
-    return acc;
-  }, [])
-);
+// Example usage:
+const matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(towelSort(matrix)); // Output: [1, 2, 3, 6, 5, 4, 7, 8, 9]
+```
